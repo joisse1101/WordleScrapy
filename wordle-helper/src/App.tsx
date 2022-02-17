7ff1c4fc2bb2bcba8e3d449753d6761cf7482d94
joisse1101 <thoiwei@gmail.com>
2022-02-17 11:24:28 +0800

    Change to UI to Material UI

--- wordle-helper/src/App.tsx
+++ wordle-helper/src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App example with TypeScript
+        </Typography>
+      </Box>
+    </Container>
   );
 }
 
