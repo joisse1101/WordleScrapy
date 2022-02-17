3820b9ce59a2b7257a78daa4dfb625a02d85b498
joisse1101 <thoiwei@gmail.com>
2022-02-17 17:11:06 +0800

    Add theme wrapper

--- wordle-helper/src/App.tsx
+++ wordle-helper/src/App.tsx
@@ -3,15 +3,21 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
+import { ThemeProvider } from '@mui/material/styles';
+import { lightTheme, darkTheme } from './common/theme';
+import Theme from './common/theme';
+
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example with TypeScript
+    <ThemeProvider theme={Theme()}>
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Create React App example with TypeScript
         </Typography>
-      </Box>
-    </Container>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ wordle-helper/src/common/theme.tsx
@@ -0,0 +1,47 @@
+import { createTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    text: {
+      primary: '#000000',
+      secondary: '#ffffff'
+    },
+    secondary: {
+      dark: '#c9b458',
+      light: '#6aaa64',
+      main: '#787c7e',
+    },
+
+    background: {
+      paper: '#d3d6da',
+      default: '#ffffff'
+    }
+  },
+});
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    text: {
+      primary: '#ffffff',
+      secondary: '#ffffff'
+    },
+    secondary: {
+      dark: '#b59f3b',
+      light: '#538d4e',
+      main: '#3a3a3c',
+    },
+
+    background: {
+      paper: '#818384',
+      default: '#121213'
+    },
+  },
+});
+
+export default function Theme() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  return (prefersDarkMode ? darkTheme : lightTheme)
+}
